6af00fe5302c71114cb9eb58148e1faad57cb068
adil-innovation-lab <khanadilaltaf@gmail.com>
2021-04-30 23:27:57 -0700

    Delete, Update, & Search
    Added functionality to delete users, update users, and search users

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -37,7 +37,9 @@ const ContactState = props => {
                 phone: '333-333-3333',
                 type: 'professional'
             }
-        ]
+        ],
+        current: null,
+        filtered: null
     }
 
     const [state, dispatch] = useReducer(contactReducer, initialState);
@@ -45,26 +47,52 @@ const ContactState = props => {
     // Add Contact
     const addContact = contact => {
         contact.id = uuid();
-        dispatch({ type: ADD_CONTACT, payload: contact })
-    }
+        dispatch({ type: ADD_CONTACT, payload: contact });
+    };
 
     // Delete Contact
+    const deleteContact = id => {
+        dispatch({ type: DELETE_CONTACT, payload: id });
+    };
 
     // Set Current Contact
+    const setCurrent = contact => {
+        dispatch({ type: SET_CURRENT, payload: contact })
+    }
 
     // Clear Current Contact
+    const clearCurrent = () => {
+        dispatch({ type: CLEAR_CURRENT })
+    }
 
     // Update Contact
+    const updateContact = contact => {
+        dispatch({ type: UPDATE_CONTACT, payload: contact })
+    }
 
     // Filter Contacts
+    const filterContacts = text => {
+        dispatch({ type: FILTER_CONTACTS, payload: text })
+    }
 
     // Clear Filter
+    const clearFilter = () => {
+        dispatch({ type: CLEAR_FILTER })
+    }
 
     return (
         <contactContext.Provider
             value={{
-                        contacts: state.contacts ,
-                        addContact
+                        contacts: state.contacts,
+                        current: state.current,
+                        filtered: state.filtered,
+                        addContact,
+                        updateContact,
+                        deleteContact,
+                        setCurrent,
+                        clearCurrent,
+                        filterContacts,
+                        clearFilter
                     }}
         >
             {props.children}
