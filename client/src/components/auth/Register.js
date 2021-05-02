d278519b77420c778a046899e19c7a864f77d350
adil-innovation-lab <khanadilaltaf@gmail.com>
2021-05-02 00:01:42 -0700

    Added Alert State and Auth State for User Registration
    Added Alert State and Auth State for User Registration

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,71 @@
+import React, { useState, useContext, useEffect } from 'react';
+import AlertContext from '../../context/alert/alertContext';
+import AuthContext from '../../context/auth/authContext';
+
+const Register = () => {
+    const alertContext = useContext(AlertContext);
+    const authContext = useContext(AuthContext);
+
+    const { setAlert } = alertContext;
+    const { register, error, clearErrors } = authContext;
+
+    useEffect(() => {
+        if(error === 'A User with this email already exists.') {
+            setAlert(error, 'danger');
+            clearErrors();
+        }
+    }, [error])
+
+    const [user, setUser] = useState({
+        name: '',
+        email: '',
+        password: '',
+        password2: ''
+    });
+
+    const { name, email, password, password2 } = user;
+
+    const onChange = e => setUser({...user, [e.target.name]: e.target.value});
+
+    const onSubmit = e => {
+        e.preventDefault();
+        if(name === '' || email === '' || password === '') {
+            setAlert('Please enter all fields', 'danger');
+        } else if (password !== password2) {
+            setAlert('Password and Confirm Password do not match', 'danger');
+        } else {
+            register({
+                name,
+                email,
+                password
+            })
+        }
+    }
+
+    return (
+        <div className='form-container'>
+            <h1>User Registration</h1>
+            <form onSubmit={onSubmit} >
+                <div className="form-group">
+                    <label htmlFor="name">Name</label>
+                    <input type="text" name="name" value={name} onChange={onChange} />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" value={email} onChange={onChange} />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" value={password} onChange={onChange} minLength="6"/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password2">Confirm Password</label>
+                    <input type="password" name="password2" value={password2} onChange={onChange} minLength="6" />
+                </div>
+                <input type="submit" value="Register" className="btn btn-primary btn-block" />
+            </form>
+        </div>
+    )
+};
+
+export default Register;
---
+++ client/src/context/auth/AuthState.js
@@ -0,0 +1,69 @@
+import React, { useReducer } from 'react';
+import axios from 'axios';
+import authContext from './authContext';
+import authReducer from './authReducer';
+
+import {
+    REGISTER_SUCCESS,
+    REGISTER_FAIL,
+    USER_LOADED,
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+    LOGOUT,
+    CLEAR_ERRORS
+} from '../types';
+
+const AuthState = props => {
+    const initialState = {
+        token: localStorage.getItem('token'),
+        isAuthenticated: null,
+        loading: true,
+        user: null,
+        error: null
+    }
+
+    const [state, dispatch] = useReducer(authReducer, initialState);
+
+    // Load User
+
+    // Register User
+    const register = async formData => {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        try {
+            const res = await axios.post('/api/users', formData, config);
+            dispatch({ type: REGISTER_SUCCESS, payload: res.data })
+        } catch (err) {
+            dispatch({ type: REGISTER_FAIL, payload: err.response.data.msg })
+        }
+    }
+
+    // Login User
+
+    // Logout User
+
+    // Clear Errors
+    const clearErrors = () => dispatch({ type: CLEAR_ERRORS })
+
+    return (
+        <authContext.Provider
+            value={{
+                        token: state.token,
+                        isAuthenticated: state.isAuthenticated,
+                        loading: state.loading,
+                        user: state.user,
+                        error: state.error,
+                        register,
+                        clearErrors
+                    }}
+        >
+            {props.children}
+        </authContext.Provider>
+    )
+}
+
+export default AuthState;
